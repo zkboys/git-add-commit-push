0c4c6a97e560663ea3859a55807ed637b7da6c97
zkboys <876213774@qq.com>
2020-05-16 12:18:23 +0800

    :hammer: 代码重构

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ try {
     const branch = execSync('git branch');
     const currentBranch = branch.toString().replace('*', '').trim();
 
-    console.log('🐎 git pull');
+    console.log('⬇️ git pull');
     execSync(`git pull`, {stdio: [0, 1, 2]});
     console.log(); // 换行
 
