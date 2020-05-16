eb2629d882e6a9a3d6c30471407c549d78ee62a5
zkboys <876213774@qq.com>
2020-05-16 10:49:54 +0800

    提交呢 这次修改有点牛逼 就是的

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const {execSync} = require('child_process');
 const program = require('commander');
 program
     .version(require('./package').version)
-    .usage('[message]')
+    .usage(`
+        [message]
+        空格 转换为换换
+    `)
     .option('-m, --message <注释>  ', '提交注释')
     .parse(process.argv);
 
@@ -13,7 +16,7 @@ let message = program.message;
 
 if (!message) {
     const [, , ...messages] = process.argv;
-    message = messages.join(' ') || '整理代码';
+    message = messages.join('\n') || '整理代码';
 }
 
 execSync(`git add . && git commit -m '${message}' && git push origin master `, {stdio: [0, 1, 2]});
