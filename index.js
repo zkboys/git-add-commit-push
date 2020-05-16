b2d1854deb7ea63e958685b16547efae2246be4a
zkboys <876213774@qq.com>
2020-05-16 10:54:47 +0800

    :bug:整理代码

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ program
     .version(require('./package').version)
     .usage(`
         [message]
-        空格 转换为换换
+        
+        说明：
+        空格 转为换行
+        
+        提交类型：
+        feat：新功能（feature）
+        fix：修补bug
+        docs：文档（documentation）
+        style： 格式（不影响代码运行的变动）
+        refactor：重构（即不是新增功能，也不是修改bug的代码变动）
+        test：增加测试
+        chore：构建过程或辅助工具的变动
     `)
     .option('-m, --message <注释>  ', '提交注释')
     .parse(process.argv);
@@ -16,7 +27,7 @@ let message = program.message;
 
 if (!message) {
     const [, , ...messages] = process.argv;
-    message = messages.join(' ') || '整理代码';
+    message = messages.join('\n') || ':bug:整理代码';
 }
 
 execSync(`git add . && git commit -m '${message}' && git push origin master `, {stdio: [0, 1, 2]});
