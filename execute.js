302f244e574971b88ea8fb6fe67968fb73126f3d
zkboys <876213774@qq.com>
2022-09-24 16:43:49 +0800

    :sparkles: 提交依稀 啊打发 啊打发 阿打算放

--- execute.js
+++ execute.js
@@ -1,24 +1,24 @@
 #! /usr/bin/env node
 
 // 快速push 到git服务器脚本
-const {execSync} = require('child_process');
+const { execSync } = require('child_process');
 const program = require('commander');
 const chalk = require('chalk');
 
-module.exports = function (pull) {
+module.exports = function(pull) {
     const types = [
-        {name: 'first', emoji: '🎉', emojiCode: 'tada', description: 'Initial commit'},
-        {name: 'feat', emoji: '✨', emojiCode: 'sparkles', description: ' 添加新功能'},
-        {name: 'feature', emoji: '✨', emojiCode: 'sparkles', description: ' 添加新功能'},
-        {name: 'fix', emoji: '🚑', emojiCode: 'ambulance', description: 'Bug修复'},
-        {name: 'docs', emoji: '📝', emojiCode: 'pencil', description: '整理文档'},
-        {name: 'style', emoji: '🎨', emojiCode: 'art', description: '整理代码'},
-        {name: 'refa', emoji: '🔨', emojiCode: 'hammer', description: '代码重构'},
-        {name: 'refactor', emoji: '🔨', emojiCode: 'hammer', description: '代码重构'},
-        {name: 'test', emoji: '✅', emojiCode: 'white_check_mark', description: ' 增加测试'},
-        {name: 'chore', emoji: '🐳', emojiCode: 'whale', description: '修改构建过程或辅助工具'},
-        {name: 'deploy', emoji: '🔖', emojiCode: 'bookmark', description: '部署'},
-        {name: 'build', emoji: '🚀', emojiCode: 'rocket', description: '构建'},
+        { name: 'first', emoji: '🎉', emojiCode: 'tada', description: 'Initial commit' },
+        { name: 'feat', emoji: '✨', emojiCode: 'sparkles', description: ' 添加新功能' },
+        { name: 'feature', emoji: '✨', emojiCode: 'sparkles', description: ' 添加新功能' },
+        { name: 'fix', emoji: '🚑', emojiCode: 'ambulance', description: 'Bug修复' },
+        { name: 'docs', emoji: '📝', emojiCode: 'pencil', description: '整理文档' },
+        { name: 'style', emoji: '🎨', emojiCode: 'art', description: '整理代码' },
+        { name: 'refa', emoji: '🔨', emojiCode: 'hammer', description: '代码重构' },
+        { name: 'refactor', emoji: '🔨', emojiCode: 'hammer', description: '代码重构' },
+        { name: 'test', emoji: '✅', emojiCode: 'white_check_mark', description: ' 增加测试' },
+        { name: 'chore', emoji: '🐳', emojiCode: 'whale', description: '修改构建过程或辅助工具' },
+        { name: 'deploy', emoji: '🔖', emojiCode: 'bookmark', description: '部署' },
+        { name: 'build', emoji: '🚀', emojiCode: 'rocket', description: '构建' },
     ];
 
     const maxNameLength = Math.max(...types.map(item => item.name.length));
@@ -49,7 +49,7 @@ module.exports = function (pull) {
 
     // 转换emoji表情
     types.forEach(item => {
-        const {name, emojiCode, description} = item;
+        const { name, emojiCode, description } = item;
         for (const em of [
             `${name}:`,
             `${name}：`,
@@ -64,8 +64,8 @@ module.exports = function (pull) {
     });
 
     // 空格转换为换行 好像获取不到双空格
-    const [m1, m2, ...others] = commitMessage.split(' ');
-    commitMessage = `${m1} ${m2}\n${others.join('\n')}`;
+    // const [m1, m2, ...others] = commitMessage.split(' ');
+    // commitMessage = `${m1} ${m2}\n${others.join(' ')}`;
 
     try {
         const branch = execSync('git branch');
@@ -77,21 +77,21 @@ module.exports = function (pull) {
 
         if (pull) {
             console.log('🚚 git pull');
-            execSync(`git pull`, {stdio: [0, 1, 2]});
+            execSync(`git pull`, { stdio: [0, 1, 2] });
             console.log(); // 换行
         }
 
         // 添加所有
         console.log('✨  git add .');
-        execSync(`git add .`, {stdio: [0, 1, 2]});
+        execSync(`git add .`, { stdio: [0, 1, 2] });
         console.log(); // 换行
 
         console.log('🔥 git commit');
-        execSync(`git commit -m '${commitMessage}'`, {stdio: [0, 1, 2]});
+        execSync(`git commit -m '${commitMessage}'`, { stdio: [0, 1, 2] });
         console.log();
 
         console.log(`🚀 git push origin ${currentBranch} `);
-        execSync(`git push origin ${currentBranch} `, {stdio: [0, 1, 2]});
+        execSync(`git push origin ${currentBranch} `, { stdio: [0, 1, 2] });
         console.log();
 
         console.log('🎉 Successfully!');
